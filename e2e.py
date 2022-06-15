999d378986595572804c2d1853357d512311b510
Elazar (Ellie) <96201125+awakzdev@users.noreply.github.com>
2022-06-15 08:38:25 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import validators
 from MainScores import app
-
+import sys
 
 def test_score_service():
     url = "http://127.0.0.1:8777/"
@@ -15,9 +15,8 @@ def main_function():
     if up == True:
         print("Code 0, Website is up and running")
     else:
-        print("Error, code number -1, Website is unreachable\nActivating URL")
-        app.run(host='127.0.0.1', port=8777)
-    test_score_service()
+        print("Error, code number -1, Website is unreachable")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main_function()
