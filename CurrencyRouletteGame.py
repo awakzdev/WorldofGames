b45b9f1e8a7b32c3415bf97971c44867c05614f2
Elazar (Ellie) <96201125+awakzdev@users.noreply.github.com>
2022-06-15 08:31:24 +0300

    Update CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -8,19 +8,19 @@ def currency_roulette_game(diff):
     response = requests.get(url)
     data = response.json()
 
-    y = data['conversion_rate']
+    converted_number = data['conversion_rate']
 
     # Y will convert the rate of USD to ILS = 3.18
     # diff is equal to difficulty
-    l = random.randint(0, 50)
-    g = (y * l - (5 - diff), y * l + (5 - diff))
-    print(f"The number generated is: {l}")
-    z = int(float(input("Convert the value of the number from USD to ILS: ")))
+    number_chosen = random.randint(0, 50)
+    rate = (converted_number * number_chosen - (5 - diff), converted_number * number_chosen + (5 - diff))
+    print(f"The number generated is: {number_chosen}")
+    user_input = int(float(input("Convert the value of the number from USD to ILS: ")))
 
-    min_range = g[0]
-    max_range = g[1]
+    min_range = rate[0]
+    max_range = rate[1]
 
-    if min_range < z < max_range:
+    if min_range < user_input < max_range:
         print("True")
         return True
     else:
